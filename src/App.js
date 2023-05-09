228279def9ca667b3282eea58597c89cc7ac73ad
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-09 19:48:01 +0200

    AddMovie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -38,6 +39,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
@@ -53,6 +58,9 @@ function App() {
 
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
